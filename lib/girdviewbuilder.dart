c55877b198cd13d9f53cd999083c9b75aa007491
SuhAilManzoor <Sohailmanzoorsario@gmail.com>
2025-01-13 23:52:55 +0500

    Gridview.builder works same as listview.builder,represent 10 container with red color each row cotains 3 containers 10 top to bottom and left to right spacing, and have litle bit explaination till now where i understand gridview.builder

--- lib/girdviewbuilder.dart
+++ lib/girdviewbuilder.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-
+//Girdview.builder how its works
+// This Girdview.builder is similar to Listview.builder i as till now where i understand
+// But in previus we made girdview with the help of container to represent each container with different shape
+// We can also do same in Girdview.builder to reduce the lengthy code we can achive same like previus one but not sure about colors
+// In this File i have represent only one color red and create item count 10 means 10 grid view of red colors 
+// i will learn about them more but still now i have this knowledge of girdview builder and this all that code tells you
 class GirdViewBuilder extends StatelessWidget {
   const GirdViewBuilder({super.key});
 
@@ -7,13 +12,13 @@ class GirdViewBuilder extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: GridView.builder(
-        itemCount: 10,
+        itemCount: 10,    // means 10 grid view 
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10),
+            crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10), // each row 3 container represention and top to bottom spacing is 10 ratio i gueess or widht height and left to right 10 spacing
         itemBuilder: (context, index) {
           return Container(
-            color: Colors.red,
-          );
+            color: Colors.red, // 10 red container  
+          );                // reduced prevoius code with the help of listview.builder
         },
       ),
     );
