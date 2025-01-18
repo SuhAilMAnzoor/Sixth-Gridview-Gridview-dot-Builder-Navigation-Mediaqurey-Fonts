9aebbdb675b5068913fa7db623348184ce6f4e36
SuhAilManzoor <Sohailmanzoorsario@gmail.com>
2025-01-18 18:57:35 +0500

    static colors used for fonts used in fonts.dart file we sperate colors pallet this is good approch to define them seperately, images, colors

---
+++ lib/font_custom_colors.dart
@@ -0,0 +1,8 @@
+// Class to define custom colors
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color appBarColor = Colors.blue; // AppBar color
+  static const Color textColor = Colors.red;   // Default text color
+  static const Color scaffoldBackgroundColor = Color.fromARGB(255, 188, 218, 231); // Scaffold background color
+}
