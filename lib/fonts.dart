b2ab5e71e839ce13d149e9a3cb8eb8280ce968aa
SuhAilManzoor <Sohailmanzoorsario@gmail.com>
2025-01-18 18:52:36 +0500

    Fonts in Flutter, how to import fonts in project, Used TiltPrism for project

--- lib/fonts.dart
+++ lib/fonts.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
-
-// fonts has 2 files Format TTF and OTF
+import 'package:sixth_gridview_navigation_mediaqurey_fonts/font_custom_colors.dart';
 class Fonts extends StatelessWidget {
   const Fonts({super.key});
-// Media Qurey
+
   @override
   Widget build(BuildContext context) {
+    // MediaQuery to get screen height dynamically
+    final double spacing = MediaQuery.of(context).size.height * 0.02;
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          "My Fonts",
-          style: TextStyle(fontFamily: "Tilt", fontWeight: FontWeight.w700),
+        backgroundColor: AppColors.appBarColor, // AppBar color from AppColors
+        title: const Text(
+          "My Fonts", // Title for AppBar
         ),
       ),
-      body: Container(
-        color: Colors.red,
-        height: MediaQuery.of(context).size.height * 0.5,
-        width: MediaQuery.of(context).size.width * 0.5,
-        // get the height and width of screen
+      body: Column(
+        children: [
+          SizedBox(height: spacing), // Responsive spacing
+          const Center(
+            child: Text(
+              "This text is for test to check font style",
+              style: TextStyle(
+                fontSize: 16, // Example font size
+                color: AppColors.textColor, // Text color from AppColors
+              ),
+            ),
+          ),
+          const Text(
+            "Suhail Manzoor",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold, // Example bold style
+              color: AppColors.textColor,
+            ),
+          ),
+        ],
       ),
     );
   }
