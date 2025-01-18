f052f05183060ce4c4cc74ab40de34ebd1cc0ed5
SuhAilManzoor <Sohailmanzoorsario@gmail.com>
2025-01-18 22:45:30 +0500

    Fonts in Flutter, how to import fonts in project, Used TiltPrism for project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sixth_gridview_navigation_mediaqurey_fonts/mediaqurey.dart';
+import 'package:sixth_gridview_navigation_mediaqurey_fonts/fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ThemeData();
-    return const MaterialApp(
-      home: Mediaqurey(),
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 188, 218, 231),  // Set every scaffold background color is this.
+        fontFamily: "Tilt"   // fontFamily Style is used for all text to awoid this you have to specify each font there where you want use it
+      ),
+      home: const Fonts(),
     );
   }
 }
