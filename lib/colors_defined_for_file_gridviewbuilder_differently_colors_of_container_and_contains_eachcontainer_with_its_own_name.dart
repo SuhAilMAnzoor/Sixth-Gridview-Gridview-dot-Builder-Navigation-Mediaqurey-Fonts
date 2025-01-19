278b4f80d89631620351ca4c95eac247b4289e98
SuhAilManzoor <Sohailmanzoorsario@gmail.com>
2025-01-19 12:11:11 +0500

    Colors defined for file gridviewbuilder_differentlycolors of container and contains each container with its own name

---
+++ lib/colors_defined_for_file_gridviewbuilder_differently_colors_of_container_and_contains_eachcontainer_with_its_own_name.dart
@@ -0,0 +1,15 @@
+  import 'package:flutter/material.dart';
+    // Define a list of colors with their names for gridviewbuilder_differently.....
+    // This is Best Approch we follow 
+    final List<Map<String, dynamic>> colors = [
+      {'color': Colors.red, 'name': 'Red'},
+      {'color': Colors.green, 'name': 'Green'},
+      {'color': Colors.blue, 'name': 'Blue'},
+      {'color': Colors.yellow, 'name': 'Yellow'},
+      {'color': Colors.orange, 'name': 'Orange'},
+      {'color': Colors.purple, 'name': 'Purple'},
+      {'color': Colors.cyan, 'name': 'Cyan'},
+      {'color': Colors.pink, 'name': 'Pink'},
+      {'color': Colors.teal, 'name': 'Teal'},
+      {'color': Colors.brown, 'name': 'Brown'},
+    ];
